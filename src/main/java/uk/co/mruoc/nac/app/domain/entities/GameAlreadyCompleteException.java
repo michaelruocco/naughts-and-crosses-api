0a014c61966fa3a3b33c6fd59b6f521dee96fbd7
Michael Ruocco <michael.ruocco@hotmail.com>
2023-10-20 21:48:25 +0100

    adds game test

--- src/main/java/uk/co/mruoc/nac/app/domain/entities/GameAlreadyCompleteException.java
+++ src/main/java/uk/co/mruoc/nac/app/domain/entities/GameAlreadyCompleteException.java
@@ -5,6 +5,6 @@ import java.util.UUID;
 public class GameAlreadyCompleteException extends RuntimeException {
 
     public GameAlreadyCompleteException(UUID id) {
-        super(String.format("game with id %s already complete", id.toString()));
+        super(String.format("game %s is already complete", id.toString()));
     }
 }
---
+++ src/test/java/uk/co/mruoc/nac/app/domain/entities/GameTest.java
@@ -0,0 +1,63 @@
+package uk.co.mruoc.nac.app.domain.entities;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+    private final UUID id = UUID.fromString("c470a2d6-a1db-4f7b-8d2c-e76e2332680c");
+    private final Status status = mock(Status.class);
+    private final Board board = mock(Board.class);
+
+    private final Turn turn =
+            Turn.builder().coordinates(new Coordinates(0, 0)).token('X').build();
+
+    private final Game game = Game.builder().id(id).status(status).board(board).build();
+
+    @Test
+    void shouldThrowErrorIfGameIsComplete() {
+        when(status.isComplete()).thenReturn(true);
+
+        Throwable error = catchThrowable(() -> game.take(turn));
+
+        assertThat(error)
+                .isInstanceOf(GameAlreadyCompleteException.class)
+                .hasMessage("game c470a2d6-a1db-4f7b-8d2c-e76e2332680c is already complete");
+    }
+
+    @Test
+    void shouldThrowErrorIfNotPlayersTurn() {
+        Throwable expectedError = new NotPlayersTurnException(turn.getToken());
+        doThrow(expectedError).when(status).validateIsTurn(turn.getToken());
+
+        Throwable error = catchThrowable(() -> game.take(turn));
+
+        assertThat(error).isEqualTo(expectedError);
+    }
+
+    @Test
+    void shouldUpdateStatusWhenTurnTaken() {
+        Status expectedStatus = mock(Status.class);
+        when(status.turnTaken()).thenReturn(expectedStatus);
+
+        Game updatedGame = game.take(turn);
+
+        assertThat(updatedGame.getStatus()).isEqualTo(expectedStatus);
+    }
+
+    @Test
+    void shouldUpdateBoardWhenTurnTaken() {
+        Board expectedBoard = mock(Board.class);
+        when(board.update(turn)).thenReturn(expectedBoard);
+
+        Game updatedGame = game.take(turn);
+
+        assertThat(updatedGame.getBoard()).isEqualTo(expectedBoard);
+    }
+}
