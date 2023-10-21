d56642aa4609e5d809ee12c44525bd6140f4a161
Michael Ruocco <michael.ruocco@hotmail.com>
2023-10-21 20:30:42 +0100

    Adds console board formatter test and tries to enable updating packages

---
+++ src/test/java/uk/co/mruoc/nac/app/domain/usecases/ConsoleBoardFormatterTest.java
@@ -0,0 +1,36 @@
+package uk.co.mruoc.nac.app.domain.usecases;
+
+import org.junit.jupiter.api.Test;
+import uk.co.mruoc.nac.app.domain.entities.Board;
+import uk.co.mruoc.nac.app.domain.entities.Turn;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ConsoleBoardFormatterTest {
+
+    private final ConsoleBoardFormatter formatter = new ConsoleBoardFormatter();
+
+    @Test
+    void shouldReturnBoardStateAsFormattedString() {
+        Board board = new Board()
+                .update(new Turn(0, 0, 'X'))
+                .update(new Turn(1, 0, 'O'))
+                .update(new Turn(2, 0, 'X'))
+                .update(new Turn(0, 1, 'O'))
+                .update(new Turn(1, 1, 'X'))
+                .update(new Turn(2, 1, 'O'))
+                .update(new Turn(0, 2, 'X'))
+                .update(new Turn(1, 2, 'O'))
+                .update(new Turn(2, 2, 'X'));
+
+        String formatted = formatter.format(board);
+
+        assertThat(formatted).isEqualTo(
+                """
+                            0 1 2
+                          0 X O X
+                          1 O X O
+                          2 X O X""");
+    }
+
+}
