9f4548256fc8a37023a0d6ed453a9ebd8535f527
Michael Ruocco <michael.ruocco@digital.homeoffice.gov.uk>
2023-11-08 19:02:28 +0000

    adds unit test coverage for allowed origins supplier

---
+++ app/spring/src/test/java/uk/co/mruoc/nac/app/config/AllowedOriginsSupplierTest.java
@@ -0,0 +1,45 @@
+package uk.co.mruoc.nac.app.config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.function.Supplier;
+import org.junit.jupiter.api.Test;
+
+class AllowedOriginsSupplierTest {
+
+    @Test
+    void shouldReturnMultipleCommaSeparatedOriginsAsAnArray() {
+        Supplier<String[]> supplier = new AllowedOriginsSupplier("http://localhost:8081,http://localhost:8082");
+
+        String[] origins = supplier.get();
+
+        assertThat(origins).containsExactly("http://localhost:8081", "http://localhost:8082");
+    }
+
+    @Test
+    void shouldReturnSingleOriginAsAnArray() {
+        Supplier<String[]> supplier = new AllowedOriginsSupplier("http://localhost:8081");
+
+        String[] origins = supplier.get();
+
+        assertThat(origins).containsExactly("http://localhost:8081");
+    }
+
+    @Test
+    void shouldReturnEmptyArrayForEmptyStringInput() {
+        Supplier<String[]> supplier = new AllowedOriginsSupplier("");
+
+        String[] origins = supplier.get();
+
+        assertThat(origins).isEmpty();
+    }
+
+    @Test
+    void shouldReturnEmptyArrayForNullInput() {
+        Supplier<String[]> supplier = new AllowedOriginsSupplier((String) null);
+
+        String[] origins = supplier.get();
+
+        assertThat(origins).isEmpty();
+    }
+}
