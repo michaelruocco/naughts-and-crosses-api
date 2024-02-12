9a6a9f0a4f44021a7d28791b4c4ab17ff556df77
Michael Ruocco <michael.ruocco@digital.homeoffice.gov.uk>
2024-02-12 10:36:47 +0000

    replaces activemq-classic image with activemq-artemis

--- app/spring/src/testFixtures/java/uk/co/mruoc/nac/environment/integrated/activemq/TestActiveMQContainer.java
+++ app/spring/src/testFixtures/java/uk/co/mruoc/nac/environment/integrated/activemq/TestActiveMQContainer.java
@@ -1,14 +1,15 @@
 package uk.co.mruoc.nac.environment.integrated.activemq;
 
-import org.testcontainers.activemq.ActiveMQContainer;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
-public class TestActiveMQContainer extends ActiveMQContainer {
+public class TestActiveMQContainer extends GenericContainer<TestActiveMQContainer> {
 
   private static final int STOMP_PORT = 61613;
 
   public TestActiveMQContainer() {
-    super(DockerImageName.parse("apache/activemq-classic:latest"));
+    super(DockerImageName.parse("apache/activemq-artemis:latest-alpine"));
+    withExposedPorts(STOMP_PORT);
   }
 
   public int getMappedStompPort() {
