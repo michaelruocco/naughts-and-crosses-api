4d84131ac61d5909f20187cabe92e8447409b4d0
Michael Ruocco <michael.ruocco@hotmail.com>
2024-07-04 16:05:05 +0100

    fixes sonar issues and improves test coverage

--- domain/use-cases/src/main/java/uk/co/mruoc/nac/usecases/UserNotMemberOfGroupsException.java
+++ domain/use-cases/src/main/java/uk/co/mruoc/nac/usecases/UserNotMemberOfGroupsException.java
@@ -1,8 +1,13 @@
 package uk.co.mruoc.nac.usecases;
 
 import java.util.Collection;
+import java.util.List;
 
 public class UserNotMemberOfGroupsException extends RuntimeException {
+
+  public UserNotMemberOfGroupsException(String username, String... permittedGroups) {
+    this(username, List.of(permittedGroups));
+  }
 
   public UserNotMemberOfGroupsException(String username, Collection<String> permittedGroups) {
     super(String.format("user %s is not a member of groups %s", username, permittedGroups));
