84d682a807a9fd00c972118565fd3317a2abd0f4
Michael Ruocco <michael.ruocco@hotmail.com>
2024-07-17 22:12:57 +0100

    removes unused import

--- adapter/api/src/test/java/uk/co/mruoc/nac/api/dto/ApiUserPageRequestTest.java
+++ adapter/api/src/test/java/uk/co/mruoc/nac/api/dto/ApiUserPageRequestTest.java
@@ -1,7 +1,6 @@
 package uk.co.mruoc.nac.api.dto;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Collection;
 import org.junit.jupiter.api.Test;
