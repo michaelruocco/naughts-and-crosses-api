25884969c975aec9f393158bfdc6d5fc353babe6
Michael Ruocco <michael.ruocco@hotmail.com>
2024-08-03 11:08:27 +0100

    applies spotless

--- adapter/repository/src/test/java/uk/co/mruoc/nac/repository/inmemory/SearchTermPredicateTest.java
+++ adapter/repository/src/test/java/uk/co/mruoc/nac/repository/inmemory/SearchTermPredicateTest.java
@@ -1,36 +1,34 @@
 package uk.co.mruoc.nac.repository.inmemory;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import uk.co.mruoc.nac.entities.User;
 import uk.co.mruoc.nac.entities.UserMother;
 
-import java.util.function.Predicate;
-import java.util.stream.Stream;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 class SearchTermPredicateTest {
 
-    @ParameterizedTest
-    @MethodSource("searchTermAndExpectedResult")
-    void shouldReturnExpectedResultForSearchTerm(String searchTerm, boolean expectedResult) {
-        Predicate<User> predicate = new SearchTermPredicate(searchTerm);
-        User user = UserMother.admin();
+  @ParameterizedTest
+  @MethodSource("searchTermAndExpectedResult")
+  void shouldReturnExpectedResultForSearchTerm(String searchTerm, boolean expectedResult) {
+    Predicate<User> predicate = new SearchTermPredicate(searchTerm);
+    User user = UserMother.admin();
 
-        boolean result = predicate.test(user);
+    boolean result = predicate.test(user);
 
-        assertThat(result).isEqualTo(expectedResult);
-    }
+    assertThat(result).isEqualTo(expectedResult);
+  }
 
-    private static Stream<Arguments> searchTermAndExpectedResult() {
-        return Stream.of(
-                Arguments.of("ad", true),
-                Arguments.of("ema", true),
-                Arguments.of("", true),
-                Arguments.of(null, true),
-                Arguments.of("blah", false)
-        );
-    }
+  private static Stream<Arguments> searchTermAndExpectedResult() {
+    return Stream.of(
+        Arguments.of("ad", true),
+        Arguments.of("ema", true),
+        Arguments.of("", true),
+        Arguments.of(null, true),
+        Arguments.of("blah", false));
+  }
 }
