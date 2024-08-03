554be0e1aea3060fddaf4f4f23536a8773e92377
Michael Ruocco <michael.ruocco@hotmail.com>
2024-08-03 11:03:58 +0100

    adds test coverage to search term predicate and external user synchronizer

--- domain/use-cases/src/test/java/uk/co/mruoc/nac/usecases/ExternalUserSynchronizerTest.java
+++ domain/use-cases/src/test/java/uk/co/mruoc/nac/usecases/ExternalUserSynchronizerTest.java
@@ -1,8 +1,12 @@
 package uk.co.mruoc.nac.usecases;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
@@ -40,7 +44,20 @@ class ExternalUserSynchronizerTest {
   }
 
   @Test
-  void synchronizeShouldDeleteUserIfPresentInRepositorAndNotInCognito() {
+  void shouldNotSynchronizeIfUserDoesExistInRepository() {
+    User user = UserMother.admin();
+    String username = user.getUsername();
+    when(repository.exists(username)).thenReturn(true);
+
+    synchronizer.synchronizeIfNotPresent(username);
+
+    verifyNoInteractions(externalUserService);
+    verify(repository, never()).update(any(User.class));
+    verify(repository, never()).delete(anyString());
+  }
+
+  @Test
+  void synchronizeShouldDeleteUserIfPresentInRepositoryAndNotInCognito() {
     User user = UserMother.admin();
     when(repository.getAll()).thenReturn(Stream.of(user));
     when(externalUserService.getAllUsers()).thenReturn(Stream.empty());
@@ -51,5 +68,17 @@ class ExternalUserSynchronizerTest {
     ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
     verify(repository).delete(captor.capture());
     assertThat(captor.getValue()).isEqualTo(user.getUsername());
+  }
+
+  @Test
+  void synchronizeShouldNotDeleteUserIfPresentInRepositoryAndInCognito() {
+    User user = UserMother.admin();
+    when(repository.getAll()).thenReturn(Stream.of(user));
+    when(externalUserService.getAllUsers()).thenReturn(Stream.of(user));
+    when(externalUserService.getByUsername(user.getUsername())).thenReturn(Optional.of(user));
+
+    synchronizer.synchronize();
+
+    verify(repository, never()).delete(anyString());
   }
 }
